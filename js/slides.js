06c4b3a2b7e24bee156ea74951d1858bc2ae8ae6
krgriner <krgriner@129-79-116-214.dhcp-bl.indiana.edu>
2016-05-05 16:39:20 -0400

    Final project

---
+++ js/slides.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+      $("#slides").slidesjs({
+        width: 940,
+        height: 400,
+        navigation: {
+      		active: false,
+      		effect: "slide",
+   		},
+        pagination: {
+      		active: false,
+      		effect: "slide",
+   		},
+        effect: {
+      		slide: {
+        		speed: 200,
+     
+    		},	
+    	},
+
+    });
+});
